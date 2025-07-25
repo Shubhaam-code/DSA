2b2a7b362441947e8d9cce77e58bf824076f7761
Subham Narayan <subhamnarayan11144@gmail.com>
2025-07-25 16:08:00 +0530

    dsa

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,15 +1,69 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void mergesort(int arr[],int start,int end)
+
+
+void merge(int a[],int start,int mid,int end){
+    vector<int>temp(end-start+1);
+    int left =start,right=mid+1,index=0;
+
+    while(start<=mid && right<=end){
+        if(a[left]<=a[right]){
+            temp[index]=a[left];
+            index++;
+            left++;
+        }
+        else{
+            temp[index]=a[right];
+            index++;
+            right++;
+        }
+        while(left<=mid){
+            temp[index]=a[left];
+            index++;
+            left++;
+        }
+        while(right<=end){
+            temp[index]=a[right];
+            index++;
+            right++;
+
+        }
+    }
+    for (int i = 0; i < index; i++) {
+        a[start + i] = temp[i];
+    }
+
+
+
+
+}
+
+
+void mergesort(int a[],int start,int end)
 {
-    if(start==end)
+    if(start>=end)
     {
-        return
+        return;
     }
     int mid = start+(end-start)/2;
     // LEFT
-    mergesort(arr,start,mid);
-    
+    mergesort(a,start,mid);
+    // RIGHT
+    mergesort(a,mid+1,end);
 
+    merge(a,start,mid,end);
+
+}
+
+int main(){
+    int a[5]={5,9,2,1,11};
+
+    mergesort(a,0,4);
+
+    for(int i=0;i<5;i++)
+    {
+        cout<<a[i]<<" ";
+    }
 }
