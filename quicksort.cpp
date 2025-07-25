4270befd45fbed77113ae2bcb8469f9c0ae85734
Subham Narayan <subhamnarayan11144@gmail.com>
2025-07-25 10:45:08 +0530

    quicksort

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,20 +1,47 @@
-// void quicksort(int arr[];int start,int end){
-
-//     int privot=pa
-// }
-
 
 #include <iostream>
 using namespace std;
 
-void display(int a){
-    a=10;
-    cout<<a;
+int partition(int arr[],int start,int end){
+    int pos=start;
+    
+    for(int i=start;i<end;i++)
+    {
+        if(arr[i]<arr[end])
+        {
+            swap(arr[pos],arr[i]);
+            pos++;
+        }
+        
+    }
+    swap(arr[pos], arr[end]); 
+    return pos;
+    
+    
 }
-int main(){
-    int a=5;
-    display(a);
-    cout<<endl;
-    cout<<a;
+
+void quicksort(int arr[],int start ,int end ){
+    if(start >= end) {  
+        return;
+    }
+    
+    int privot =partition(arr,  start , end);
+    
+    // left
+    quicksort(arr,start,privot-1);
+    //Right
+    quicksort(arr,privot+1,end);
+    
+    
+}
+
+
+int main() {
+    int arr[6]={5,2,8,7,1,4};
+    quicksort(arr,0,5);
+    for(int i=0;i<6;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
