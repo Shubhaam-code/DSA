fc3f221caeb151a6bd866422a9677f03d3727a57
Subham Narayan <subhamnarayan11144@gmail.com>
2025-07-30 23:25:26 +0530

    node

--- createnode.cpp
+++ createnode.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 using namespace std;
+
 class node
 {
     public:
 
   int data;
   node *next;
+  //constructor
   node(int value){
     data=value;
-    next = NULL;
+    next=NULL;
   }
+  
 
 };
-int main()
-{
-    node * head;
-    head = new node(4);
+int main(){
+  // dyanmic memory allocation
+  // pointer banao
+  node *head; // head bss name ha pointer ka or node jo likha uska name pointer point kar raha node ko
+  head =new node(4); // now node will create in heap
+  
 
-    cout<<"data">-data<<endl;
-    
-
+  cout<<head->data;
 }
