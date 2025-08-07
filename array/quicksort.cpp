8dbd3745fae8fa60f8407303e050abed62f677a4
Subham Narayan <subhamnarayan11144@gmail.com>
2025-08-07 21:35:47 +0530

    update

--- array/quicksort.cpp
+++ array/quicksort.cpp
@@ -1,46 +1,38 @@
- #include <iostream>
+// Online C++ compiler to run C++ program online
+#include <iostream>
 using namespace std;
 
-int partition(int arr[],int start,int end){
+int partition(int arr [],int start ,int end){
+
     int pos=start;
-    
-    for(int i=start;i<end;i++)
-    {
-        if(arr[i]<arr[end])
-        {
+    for(int i=start;i<=end;i++){
+        if(arr[i]<=arr[end]){
             swap(arr[pos],arr[i]);
             pos++;
         }
-        
     }
-    swap(arr[pos], arr[end]); 
-    return pos;
     
+    return pos-1;
     
 }
 
-void quicksort(int arr[],int start ,int end ){
-    if(start >= end) {  
+void quicksort(int arr [],int start ,int end){
+    
+    if(start>=end){
         return;
     }
     
-    int privot =partition(arr,  start , end);
-    
-    // left
-    quicksort(arr,start,privot-1);
-    //Right
-    quicksort(arr,privot+1,end);
-    
-    
+    int privot=partition(arr ,start,end);
+    quicksort(arr , start ,privot-1);
+    quicksort(arr , privot+1 , end);
 }
 
-
 int main() {
-    int arr[6]={5,2,8,7,1,4};
-    quicksort(arr,0,5);
-    for(int i=0;i<6;i++)
-    {
-        cout<<arr[i]<<" ";
+    int a[7]={5,8,1,3,7,2,4};
+    quicksort(a,0,6);
+    for(int i = 0; i < 7; i++) {
+        cout << a[i] << " ";
     }
+
     return 0;
 }
