2fd02173d3968f930ad3f7c9eab4a34e859843ae
Subham Narayan <subhamnarayan11144@gmail.com>
2025-08-20 00:35:06 +0530

    update

---
+++ linkedlist/create_linklist.cpp
@@ -0,0 +1,38 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node *link;
+    node(int d){
+        data=d;
+        link=NULL;
+    }
+};
+
+int main() {
+    node *start= new node(4);
+    node *first= new node(5);
+    node *second= new node(6);
+    node *third= new node(7);
+    start->link=first;
+    first->link=second;
+    second->link=third;
+    
+    
+    node *temp;
+    temp=start;
+    
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->link;
+    }
+    
+   
+    
+    
+    
+    
+}
